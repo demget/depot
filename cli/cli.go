12d0400ba6f7d9a0a9262380894e19f7ada43e3e
iskrim <cyb3r@nigge.rs>
2023-12-18 20:58:16 +0100

    cli: fix command parsing, add address validation

--- cli/cli.go
+++ cli/cli.go
@@ -7,74 +7,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type cobraCmdHandler = func(cmd *cobra.Command, args []string) error
-
-func mockStartServer(addr, path string) error {
-	fmt.Printf("Depot is sharing '%s' on address %s.\n", path, addr)
-	fmt.Printf("To connect, enter the command `depot client --addr %s`.\n", addr)
-
-	return nil
-}
-
-func mockStartClient(addr, path string) error {
-	fmt.Printf("Depot client connected to %s successfully!\n", addr)
-
-	return nil
-}
-
-func verboseCliInfo(target cobraCmdHandler) cobraCmdHandler {
-	return func(cmd *cobra.Command, args []string) error {
-		fmt.Printf("Command: %s\nArgs: %v\n\n", cmd.Name(), args)
-		return target(cmd, args)
-	}
-}
+const Version = "v0.1"
 
 func Run() {
-	var addr string
 
 	var (
 		rootCmd = &cobra.Command{
 			Use:   "depot",
 			Short: "depot",
-			RunE: verboseCliInfo(func(cmd *cobra.Command, args []string) error {
-				println("?")
-				return nil
-			}),
+			Run: func(cmd *cobra.Command, args []string) {
+				cmd.Help()
+			},
 		}
 
 		versionCmd = &cobra.Command{
 			Use:   "version",
 			Short: "Print the version number of depot",
 			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("depot v0.0")
+				fmt.Println("depot", Version)
 			},
-		}
-
-		serverCmd = &cobra.Command{
-			Use:   "server",
-			Short: "Start depot fileserver",
-			Args:  cobra.ExactArgs(1),
-			RunE: verboseCliInfo(func(cmd *cobra.Command, args []string) error {
-				return mockStartServer(addr, args[0])
-			}),
-		}
-
-		clientCmd = &cobra.Command{
-			Use:   "client",
-			Short: "Connect to depot fileserver",
-			Args:  cobra.ExactArgs(1),
-			RunE: verboseCliInfo(func(cmd *cobra.Command, args []string) error {
-				return mockStartClient(addr, args[0])
-			}),
 		}
 	)
 
 	rootCmd.AddCommand(versionCmd)
-	rootCmd.AddCommand(serverCmd)
-	rootCmd.AddCommand(clientCmd)
-
-	serverCmd.PersistentFlags().StringVar(&addr, "addr", "::1338", "Address for server to run")
-	clientCmd.PersistentFlags().StringVar(&addr, "addr", "::1338", "Address of server")
+	rootCmd.AddCommand(NewCmdServer())
+	rootCmd.AddCommand(NewCmdClient())
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
---
+++ cli/server.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func runServer(path, addr string) error {
+	err := os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = splitHostPort(addr)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Depot is sharing '%s' on address %s.\n", path, addr)
+	fmt.Printf("To connect, enter the command `depot client '%s'`.\n", addr)
+	return nil
+}
+
+func NewCmdServer() *cobra.Command {
+	var addr string
+
+	serverCmd := &cobra.Command{
+		Use:   "server",
+		Short: "Start depot fileserver",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runServer(args[0], addr)
+		},
+	}
+
+	serverCmd.PersistentFlags().StringVar(&addr, "addr", "::1338", "Address for server to run")
+
+	return serverCmd
+}
